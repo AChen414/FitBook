edaf6436f30bd11f2f8020faaddad9c18b26e7e2
Gordy <gordybullen@gmail.com>
2020-07-07 15:31:48 -0700

    Add util methods for exercise ajax requests

--- frontend/src/util/exercise_api_util.js
+++ frontend/src/util/exercise_api_util.js
@@ -1,13 +1,25 @@
 import axios from "axios";
 
-export const getTweets = () => {
-  return axios.get("/api/exercises");
+export const getExercises = () => {
+  return axios.get('/api/exercises');
 };
 
-export const getUserExercises = (id) => {
-  return axios.get(`/api/exercises/user/${id}`);
+export const getExercise = (id) => {
+  return axios.get(`/api/exercises/${id}`);
 };
 
-export const writeExercise = (data) => {
-  return axios.post("/api/exercises/", data);
+export const getUserExercises = (userId) => {
+  return axios.get(`/api/exercises/user/${userId}`);
+};
+
+export const createExercise = (data) => {
+  return axios.post('/api/exercises', data);
+};
+
+export const updateExercise = (data) => {
+  return axios.patch(`/api/exercises/${data._id}`, data);
+};
+
+export const deleteExercise = (id) => {
+  return axios.delete(`/api/exercises/${id}`);
 };
