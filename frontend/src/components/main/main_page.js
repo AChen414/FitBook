6f3840ba4c492958c5e815a2bc10a5be85796d81
Kenneth <kennethkjliang@gmail.com>
2020-07-09 14:41:38 -0700

    changed exercise index item

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -49,22 +49,22 @@ class MainPage extends React.Component {
         </div>
         <hr className="featurette-divider" />
 
-        <div class="row featurette">
-          <div class="col-md-7 col-md-push-5">
-            <h2 class="featurette-heading">
+        <div className="row featurette">
+          <div className="col-md-7 col-md-push-5">
+            <h2 className="featurette-heading">
               Oh yeah, it's that good.{" "}
-              <span class="text-muted">See for yourself.</span>
+              <span className="text-muted">See for yourself.</span>
             </h2>
-            <p class="lead">
+            <p className="lead">
               Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
               ligula porta felis euismod semper. Praesent commodo cursus magna,
               vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
               commodo.
             </p>
           </div>
-          <div class="col-md-5 col-md-pull-7">
+          <div className="col-md-5 col-md-pull-7">
             <img
-              class="featurette-image img-responsive center-block"
+              className="featurette-image img-responsive center-block"
               data-src="holder.js/500x500/auto"
               alt="500x500"
               src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDUwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzUwMHg1MDAvYXV0bwpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNi4wLgpMZWFybiBtb3JlIGF0IGh0dHA6Ly9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnM+PHN0eWxlIHR5cGU9InRleHQvY3NzIj48IVtDREFUQVsjaG9sZGVyXzE3MzM0ZTM2MWMyIHRleHQgeyBmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MjVwdCB9IF1dPjwvc3R5bGU+PC9kZWZzPjxnIGlkPSJob2xkZXJfMTczMzRlMzYxYzIiPjxyZWN0IHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIiBmaWxsPSIjRUVFRUVFIi8+PGc+PHRleHQgeD0iMTg1LjExNzE4NzUiIHk9IjI2MS4xIj41MDB4NTAwPC90ZXh0PjwvZz48L2c+PC9zdmc+"
