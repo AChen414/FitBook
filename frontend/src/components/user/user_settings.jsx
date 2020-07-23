5763106bda6d115a196f7d5d7a5a7feaed2adb52
Kenneth <kennethkjliang@gmail.com>
2020-07-23 14:29:38 -0700

    user setttings form

--- frontend/src/components/user/user_settings.jsx
+++ frontend/src/components/user/user_settings.jsx
@@ -4,7 +4,17 @@ class UserSettingsForm extends React.Component{
     constructor(props){
         super(props)
         this.state= {
-            
+
         }
     }
+    
+    render () {
+        return (
+            <div>
+                hello
+            </div>
+        )
+    }
 }
+
+export default UserSettingsForm
