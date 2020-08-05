c05c0da15a208fb1246c97be3ae6e8752cfee3fc
Andrew Chen <achen414@gmail.com>
2020-08-05 13:19:36 -0700

    Create default exercises

---
+++ models/default_exercises.js
@@ -0,0 +1,13 @@
+const defaultExercises = [
+    { user, title: "Bench Press", category: "Chest", equipment: "Barbell", notes: "" },
+    { user, title: "Squat", category: "Legs", equipment: "Barbell", notes: "" },
+    { user, title: "Deadlift", category: "Full-Body", equipment: "Barbell", notes: "" },
+    { user, title: "Shoulder Press", category: "Shoulders", equipment: "Barbell", notes: "" },
+    { user, title: "Incline Bench Press", category: "Chest", equipment: "Barbell", notes: "" },
+    { user, title: "Dumbell Curls", category: "Arms", equipment: "Dumbells", notes: "" },
+    { user, title: "Lat Pulldowns", category: "Back", equipment: "Machine", notes: "" },
+    { user, title: "Skull Crushers", category: "Arms", equipment: "Barbell", notes: "" },
+    { user, title: "Pushups", category: "Chest", equipment: "None", notes: "" }
+]
+
+module.exports = defaultExercises;
