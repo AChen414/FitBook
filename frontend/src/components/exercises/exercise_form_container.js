466397588d6ebfe49c5de7e9167e8072d969c3a8
Kenneth <kennethkjliang@gmail.com>
2020-08-06 23:07:20 -0700

    removed unsued modal action

--- frontend/src/components/exercises/exercise_form_container.js
+++ frontend/src/components/exercises/exercise_form_container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { openModal, closeModal } from '../../actions/modal_actions';
+import { closeModal } from '../../actions/modal_actions';
 import { composeExercise } from '../../actions/exercise_actions';
 
 import ExerciseForm from './exercise_form';
