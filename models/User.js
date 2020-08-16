260109300d4d770644398fbee12d6dfe331d0ee2
Kenneth <kennethkjliang@gmail.com>
2020-08-16 15:36:19 -0700

    chagned profilePhotoKey to profilePhotoLink

--- models/User.js
+++ models/User.js
@@ -1,24 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    profilePhotoKey: {
-        type: String
-    }
-}, {
-    timestamps: true
-})
+    profilePhotoLink: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = User = mongoose.model('User', UserSchema);
