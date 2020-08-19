9ffbeaffe2ee0d51cf20a4e4b0c5029097be6549
Kenneth <kennethkjliang@gmail.com>
2020-08-18 23:36:57 -0700

    corrected User model

--- models/User.js
+++ models/User.js
@@ -15,7 +15,7 @@ const UserSchema = new Schema(
       type: String,
       required: true,
     },
-    profilePhotoKey: {
+    profilePhotoLink: {
         type: String
     },
       calendarData: {
